9600cdceac54e8996dba9fbc0bb6aba6f1d0663b
Lucas Seinfeld <luchaos@gmail.com>
2013-02-27 19:58:13 +0100

    using working_dir (project_path:folder) to use main sublime project folder added "build by selected file" feature to build from another project folder

--- SuperAnt_exec.py
+++ SuperAnt_exec.py
@@ -8,16 +8,29 @@ DEFAULT_BUILD_TASK = "build"
 class SuperAntExecCommand(sublime_plugin.WindowCommand):
     def run(self, **kwargs):
         package_dir = os.path.join(sublime.packages_path(), "Super Ant");
-        self.build = os.path.join(package_dir, 'build.xml')
+        
+        self.working_dir = kwargs['working_dir'];
+        self.build = None;
 
         s = sublime.load_settings("SuperAnt.sublime-settings");
         build_file = s.get("build_file", "build.xml");
 
-        path = None;
-        if len(self.window.folders()) > 0:
-            for folder in self.window.folders():
-                if os.path.exists(folder + os.sep + build_file):
-                    self.build = folder + os.sep + build_file;
+        # buildfile by selection: search build file in project folder that file from active view is in  
+        try:
+            active_file = self.window.active_view().file_name();
+            active_folder = os.path.dirname(active_file);
+            if os.path.exists(active_folder + os.sep + build_file):
+                self.build = active_folder + os.sep + build_file;
+                self.working_dir = active_folder
+            else:
+                raise Exception('not a build file');
+        except Exception as ex:
+            print 'No build file in base folder of currently viewed file';
+
+        # buildfile by default: build.xml found in first project folder
+        if self.build == None and os.path.exists(self.working_dir + os.sep + build_file):
+            self.build = self.working_dir + os.sep + build_file;
+
         try:
             f = open(self.build);
         except Exception as ex:
@@ -25,40 +38,38 @@ class SuperAntExecCommand(sublime_plugin.WindowCommand):
             self.window.open_file(os.path.join(package_dir, 'SuperAnt.sublime-settings'));
             return 'The file could not be opened';
     
+        self.working_dir = os.path.dirname(self.build);
+
         data = f.read();
         dom = parseString(data);
-        targets = dom.getElementsByTagName('target');
+        self.targets = dom.getElementsByTagName('target');
 
         self.targetsList = [];
-        for target in targets:
+        for target in self.targets:
             targetName = target.getAttributeNode("name").nodeValue;
             if targetName[0] != "_":
-                self.targetsList.append(target.getAttributeNode("name").nodeValue);
+                self.targetsList.append(os.path.basename(self.working_dir)+': '+target.getAttributeNode("name").nodeValue);
 
         self.targetsList = sorted(self.targetsList);
 
         self.window.show_quick_panel(self.targetsList, self._quick_panel_callback);
 
     def _quick_panel_callback(self, index):
+
         if (index > -1):
-            targetName = self.targetsList[index];
+            targetName = self.targets[index].getAttributeNode("name").nodeValue;
             print targetName;
             
             ant = "ant";
             # Check for Windows Overrides and Merge
             if sys.platform.startswith('win32'):
-                    ant = "ant.bat";
+                ant = "ant.bat";
 
-            path = None;
-            if len(self.window.folders()) > 0:
-                path = self.window.folders()[0];
-                
-            if path != None:
-                cmd = {
-                    'cmd': [ant, "-f", self.build, targetName],
-                    'working_dir': path
-                }
+            cmd = {
+                'cmd': [ant, "-f", self.build, targetName],
+                'working_dir': self.working_dir
+            }
 
-                print cmd;
-                # run build
-                self.window.run_command("exec", cmd);
+            print cmd;
+            # run build
+            self.window.run_command("exec", cmd);
